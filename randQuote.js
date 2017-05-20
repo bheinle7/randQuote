88793a6837dc5eab4853a1e475462568e266d009
Benjamin Heinle <bheinle7@gmail.com>
2017-05-20 16:27:40 -0500

    js In House
    Replaced the tweet function with my own built function rater than the tweet function provided by Twitter

--- randQuote.js
+++ randQuote.js
@@ -1,28 +1,27 @@
 $(document).ready(function() {
+  var quoteCleaned = "";
   $("#getQuote").on("click", function() {
     $.ajax({
       url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=",
       success: function(a) {
-        $(".quote")
+        $("#quote")
           .empty()
           .append(a[0].content + "<p>&mdash; " + a[0].title + "</p>");
-          var quote = a[0].content
-          var quotee = a[0].title
-          var divQuote = document.createElement('div');
-          var divQuotee = document.createElement('div');
-          divQuote.innerHTML = quote;
-          divQuotee.innerHTML = quotee;
-          var quoteCleaned = divQuote.textContent
-          var quoteeCleaned = divQuotee.textContent
+        var quote = a[0].content;
+        var quotee = a[0].title;
+        var divQuote = document.createElement("div");
+        var divQuotee = document.createElement("div");
+        divQuote.innerHTML = quote;
+        divQuotee.innerHTML = quotee;
+        var quoteCleaned = divQuote.textContent;
+        var quoteeCleaned = divQuotee.textContent;
 
-        $('#tweet iframe').remove();
-          var tweet = $('<a></a>')
-          .addClass('twitter-share-button')
-          .attr('data-text', quoteCleaned+'-'+quoteeCleaned)
-          .attr('data-url',' ');
-        $('#tweet').append(tweet);
-        twttr.widgets.load();
-
+        var tweetBtn = document.getElementById("tweet");
+        tweetBtn.onclick = function() {
+          window.open(
+            "https://twitter.com/share?text=" + quoteCleaned + " -" + quoteeCleaned + "&url=blank"
+          );
+        };
       },
       cache: false
     });
