6940ce5588bf93b81f435fb05fc2d6176d7987ba
Benjamin Heinle <bheinle7@gmail.com>
2017-06-20 08:29:12 -0500

    update for hosting on git pages

--- randQuote.js
+++ randQuote.js
@@ -1,29 +1,22 @@
 $(document).ready(function() {
-  var quoteCleaned = "";
+  var quote = "";
   $("#getQuote").on("click", function() {
     $.ajax({
-      url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=",
-      success: function(a) {
-        $("#quote")
-          .empty()
-          .append(a[0].content + "<p>&mdash; " + a[0].title + "</p>");
-        var quote = a[0].content;
-        var quotee = a[0].title;
-        var divQuote = document.createElement("div");
-        var divQuotee = document.createElement("div");
-        divQuote.innerHTML = quote;
-        divQuotee.innerHTML = quotee;
-        var quoteCleaned = divQuote.textContent;
-        var quoteeCleaned = divQuotee.textContent;
-
-        var tweetBtn = document.getElementById("tweet");
-        tweetBtn.onclick = function() {
-          window.open(
-            "https://twitter.com/share?text=" + quoteCleaned + " -" + quoteeCleaned + "&url=blank"
-          );
-        };
+      url: "http://quotes.stormconsultancy.co.uk/random.json",
+      dataType: "json",
+      success: function (response) {
+        if (response.author) {
+          $("#quote").text(response.quote + " - " + response.author);
+          quote = response.quote + " - " + response.author;
+        } else {
+          $("#quote").text(response.quote + " - " + "Unkown");
+          quote = response.quote + " - " + "Unkown";
+        }
       },
       cache: false
     });
+  });
+  $("#tweet").on("click", function () {
+    window.open("https://twitter.com/share?text=" + quote + "&url=blank");
   });
 });
